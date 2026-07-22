c232f517c224f777c748499df0032f7cc0f2d0d2
Abhishek01 <etamabhishek000@gmail.com>
2026-07-22 17:03:39 +0530

    22

---
+++ support-ai/src/components/HomeClient.tsx
@@ -0,0 +1,30 @@
+'use client'
+import React from 'react'
+import { motion } from 'motion/react'
+
+function HomeClient() {
+    return (
+        <div className='min-h-screen bg-linear-to-br from-white to-zinc-50 text-zinc-900 overflow-x-hidden'>
+            <motion.div
+            // this is a nav bar 
+            initial={{y:-20}}
+            animate={{y:0}}
+            transition={{duration:1}}
+                className='fixed top-0 left-0 w-full z-50 bg-white/70 backdrop-blur-xl border-b border-zinc-200'
+            >
+                <div className='max-w-7xl max-auto px-6 h-16 flex items-center justify-between'>
+                    <div className='text-lg font-semibold tracking-tight'>Support <span className='text-zinc-400'>Ai</span></div>
+                    <button // this is login button
+                        className='px-5 py-2 rounded-full
+                    bg-black text-white text-sm font-medium
+                    hover:bg-zinc-800 transition
+                    disable:opacity-60
+                    flex items-center gap-2'
+                    >Login</button>
+                </div>
+            </motion.div>
+        </div>
+    )
+}
+
+export default HomeClient
